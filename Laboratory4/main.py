7700c45dac7c5586c231cdae7fc0ff864bacdaa4
DideaBogdan <dideabogdan@gmail.com>
2022-10-26 23:31:59 +0300

    lab4

--- Laboratory4/main.py
+++ Laboratory4/main.py
@@ -5,14 +5,30 @@ import pymc3 as pm
 import arviz as az
 
 model = pm.Model()
-alpha = 0.5
+
 
 with model:
+    alpha = 4.7
     traffic = pm.Poisson("T", mu=20)
-    order = pm.Normal("O", 1, sigma=0.5)
+    order = pm.Normal("O", mu=1, sigma=0.5)
     cook = pm.Exponential("C", lam=alpha)
-    trace = pm.sample(2000, chains=1)
+    trace = pm.sample(500, chains=1)
 
+dictionary = {
+    "T_C": trace["C"].tolist(),
+    "O" : trace["O"].tolist()
+}
+df = pd.DataFrame(dictionary)
 
-az.plot_posterior(trace)
+wait_time = df[(df["O"] + df["T_C"] <= 15)]
+print("Timpul de asteptare <= 15:", wait_time.shape[0] / df.shape[0])
+
+total = 0
+for tup in zip(df["O"], df["T_C"]):
+    total += tup[0] + tup[1]
+
+print("Timpul mediu de asteptare:", total / df.shape[0])
+
+az.plot_posterior({"Clienti pe ora": trace["T"], "Timp de asteptare": trace["O"],
+                   "Medie comenzi": trace["C"]})
 plt.show()
